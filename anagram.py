9a41c718ab763f69c19da01bc10707750bc0d0ee
Cameron Smith <cameronsmith@email.arizona.edu>
2016-10-06 15:50:55 -0700

    updated anagram. -1 byte

--- anagram.py
+++ anagram.py
@@ -1,1 +1,9 @@
-w=input();x=w.split(', ');print(sorted(x[0])==sorted(x[1]))
+"""Takes two words sperated by a comma and space. 
+
+If the words are anagrams of each other, print True. 58 bytes.
+
+Idea to save functions to variable from: 
+http://codegolf.stackexchange.com/a/1314/60037 
+Saved 1 byte
+"""
+w=input();x=w.split(', ');S=sorted;print(S(x[0])==S(x[1]))
